ec3a7a98253600290b23ead7f28c4e3e0e6c3446
Vano <simplmd@gmail.com>
2024-12-24 23:29:32 +0700

    dev

---
+++ source/teled/telegram/user.d
@@ -0,0 +1,26 @@
+module teled.telegram.user;
+import std.json;
+import std.typecons;
+import std;
+
+
+struct User
+{
+    ulong    id;
+    bool   is_bot;
+    string first_name;
+
+    Nullable!string last_name;
+    Nullable!string username;
+    Nullable!string language_code;
+
+    this(string data) 
+    {
+        JSONValue dataJson = parseJSON(data)["result"];
+        writeln(dataJson["id"]);
+        this.id = dataJson["id"].get!ulong;
+        this.is_bot = dataJson["is_bot"].get!bool;
+        this.first_name = dataJson["first_name"].get!string();
+        this.username = dataJson["username"].get!string();
+    }
+}

e75c2afa1b2cbc1dcf819249436c57c9b7d110d2
Vano <simplmd@gmail.com>
2024-12-26 17:58:55 +0700

    dev

---
+++ source/teled/packade.d
@@ -0,0 +1,17 @@
+module teled.packade;
+
+public import teled.core.drivers.http;
+public import teled.core.drivers.rawapi;
+public import teled.core.drivers.vibeclient;
+public import teled.core.metods;
+public import teled.core.options;
+public import teled.telegram.chat;
+public import teled.telegram.content;
+public import teled.telegram.markup;
+public import teled.telegram.message;
+public import teled.telegram.metods;
+public import teled.telegram.pay;
+public import teled.telegram.stiker;
+public import teled.telegram.update;
+public import teled.telegram.user;
+public import teled.bot;
